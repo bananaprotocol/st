140bee1bee6afd5cbb81a0b6187ad329ea7786f8
bananaprotocol <bananaprotocol@protonmail.com>
2019-11-27 21:34:09 +0100

    Fix color references of the gruvbox themes

--- themes/gruvbox-light.h
+++ themes/gruvbox-light.h
@@ -25,7 +25,6 @@ static const char *colorname[] = {
      	/* more colors can be added after 255 to use with DefaultXX */
         "#fbf1c7",
      	"#3c3836",
-     	"#3c3836",
 };
      
      
@@ -33,7 +32,7 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultbg = 256;
 unsigned int defaultfg = 257;
-static unsigned int defaultcs = 258;
-static unsigned int defaultrcs = 258;
+unsigned int defaultbg = 256;
+static unsigned int defaultcs = 257;
+static unsigned int defaultrcs = 0;
